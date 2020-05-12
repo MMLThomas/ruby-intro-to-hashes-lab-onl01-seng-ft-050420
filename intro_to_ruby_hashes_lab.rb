9f23e8eb1e9760484965c9cf36a42a7c175d9883
michael thomas <michael.thomas2014@gmail.com>
2020-05-12 05:48:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ end
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
   monopoly = {railroads: {pieces: 4, names: {}}}
+  binding.pry
 end
 
 def monopoly_with_fourth_tier
